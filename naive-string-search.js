4af905d02d64c98175f48e22239dc6d394f20631
HyeonJu <hhjc017@gmail.com>
2022-10-14 16:52:06 +0900

    문자열 검색(Naive String Search) 강의 내용 정리

--- naive-string-search.js
+++ naive-string-search.js
@@ -26,3 +26,24 @@ function stringSearch(string = '', pattern) {
 }
 
 console.log(stringSearch('hello world! world! hahahaha', 'world'))
+
+/*
+강의 내용)
+1. 반복문 내의 break
+    - 아래 예제에서 inner loop에 break이 사용되었다. 
+    - break를 실행하게 되면, j는 0으로 초기화 되고 i++ 된다. 
+2. j === pattern.length -1 은 
+    break 없이 모든 inner loop가 실행되었음 === pattern에 해당하는 문자열을 찾았음을 의미한다. 
+*/
+
+function stringSearch2(string = '', pattern = '') {
+  let count = 0
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      if (pattern[j] !== string[i + j]) break
+      if (j === pattern.length - 1) count++
+    }
+  }
+  return count
+}
+console.log(stringSearch2('hello world! world! hahahaha', 'ha'))
